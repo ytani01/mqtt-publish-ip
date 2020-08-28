4e51650cc7dea403b6ee8c013dab2b7950d606ca
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2020-08-29 01:26:57 +0900

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# MQTT Publish IP 
+
+自分のIPアドレスをMQTTで、Publish(公開)します。
+
+## Install
+
+```bash
+$ cd ~
+$ python3 -m venv env1
+$ . ~/env1/bin/activate
+(env1)$ cd ~/env1
+(env1)$ git clone git@mqtt-github.com:ytani01/publish-ip.git
+(env1)$ cd publish-ip
+(env1)$ ./setup.sh
+```
---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#
+# (c) 2020 Yoichi Tanibayashi
+#
+GITS="ytMQTT"
+GITADDR="git@github.com:ytani01"
+
+##############################################################################
+MYNAME=`basename $0`
+MYDIR=`dirname $0`
+echo "MYDIR=$MYDIR"
+cd $MYDIR
+echo "===" `pwd`
+##############################################################################
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo "$MYNAME: Please activate venv"
+    exit 1
+fi
+VENVNAME=`basename $VIRTUAL_ENV`
+echo "VENVNAME=$VENVNAME"
+##############################################################################
+# update pip
+echo "=== update pip command"
+set -x
+pip install -U pip
+hash -r
+pip -V
+set +x
+##############################################################################
+# install python packages
+echo "=== install python packages"
+set -x
+pip install -r requirements.txt
+set +x
+##############################################################################
+# git clone
+cd $VIRTUAL_ENV
+echo "=== git clone repos in" `pwd`
+for g in $GITS; do
+    if [ ! -d $g ]; then
+        set -x
+        git clone "${GITADDR}/${g}.git"
+        set +x
+    fi
+done
